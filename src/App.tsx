93ee6d42ed2af83e8189db9e48c5b03b12eca8b3
gewentz <gabrielfwtz@gmail.com>
2024-07-18 16:47:31 -0300

    added lucide-react and updated menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
+import { Home, NotepadTextDashed, Receipt, ScrollText, Warehouse } from 'lucide-react';
 import React from 'react';
 
 const App: React.FC = () => {
   return (
-  <div className='w-full bg-zinc-900 h-full text-zinc-100 flex items-center justify-center p-2'>
     <div id='main-frame' className='w-full bg-zinc-600 h-screen rounded-md shadow-2xl p-3 flex flex-row gap-2'>
-      
-      <div id='menu-open' className=' rounded-sm bg-zinc-500 w-52 p-3 flex flex-col gap-4 items-center h-auto z-20'>
-        <button className='border border-blue-400 bg-slate-600 text-zinc-100 rounded-md w-full hover:bg-slate-800 hover:text-blue-400 h-9'>Inicio</button>
-        <button className='border border-blue-400 bg-slate-600 text-zinc-100 rounded-md w-full hover:bg-slate-800 hover:text-blue-400 h-9'>Estoque</button>
-        <button className='border border-blue-400 bg-slate-600 text-zinc-100 rounded-md w-full hover:bg-slate-800 hover:text-blue-400 h-9'>Vendas</button>
-        <button className='border border-blue-400 bg-slate-600 text-zinc-100 rounded-md w-full hover:bg-slate-800 hover:text-blue-400 h-9'>Relatórios</button>
-        <button className='border border-blue-400 bg-slate-600 text-zinc-100 rounded-md w-full hover:bg-slate-800 hover:text-blue-400 h-9'>NF-e</button>
-      </div>
 
-      <div id='screen-content' className=' rounded-sm bg-zinc-500 w-full flex-grow p-3'>conteudo</div>
+      <div id='screen-content' className=' rounded-sm bg-zinc-500 w-full flex-grow p-3'>
+
+        <div id='menu-open' className=' rounded-sm bg-zinc-500 w-full p-3 flex flex-row gap-2 justify-center items-center h-auto z-20 border float-left'>
+          <button className=' border border-blue-400 bg-slate-600 text-zinc-100 rounded-md w-52 hover:bg-slate-800 hover:text-blue-400 h-9 flex flex-nowrap gap-4 justify-center items-center'><Home/> Inicio</button>
+          <button className=' border border-blue-400 bg-slate-600 text-zinc-100 rounded-md w-52 hover:bg-slate-800 hover:text-blue-400 h-9 flex flex-nowrap gap-4 justify-center items-center'><Warehouse/> Estoque</button>
+          <button className=' border border-blue-400 bg-slate-600 text-zinc-100 rounded-md w-52 hover:bg-slate-800 hover:text-blue-400 h-9 flex flex-nowrap gap-4 justify-center items-center'><Receipt/> Vendas</button>
+          <button className=' border border-blue-400 bg-slate-600 text-zinc-100 rounded-md w-52 hover:bg-slate-800 hover:text-blue-400 h-9 flex flex-nowrap gap-4 justify-center items-center'><NotepadTextDashed/> Relatórios</button>
+          <button className=' border border-blue-400 bg-slate-600 text-zinc-100 rounded-md w-52 hover:bg-slate-800 hover:text-blue-400 h-9 flex flex-nowrap gap-4 justify-center items-center'><ScrollText/> NF-e</button>
+        </div>
+        1 teste teste teste testes teste teste teste testes teste teste teste testes teste teste teste testes teste teste teste testes teste teste teste testes teste teste teste testes teste teste teste testes teste teste teste testes teste teste teste testes teste teste teste testes teste teste teste testes teste teste teste testes teste teste teste testes
+      </div>
     </div>
-  </div>
+
 );
 };
 
